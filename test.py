89a5c1f621ac9b260f1289f95b96add4b659cf11
Aqu212 <Aqu212@gmail.com>
2010-11-07 02:23:45 +0000

    add LPLSendControl to OMACC.nc

--- test.py
+++ test.py
@@ -29,26 +29,14 @@ initNodes()
 parent = [1]
 parent.append(2)
 
-for i in range(1,B):
-	cskipd=(1-B*math.pow(B, toth-2))/(1-B)
-	r.add(int(parent[0]), int(parent[0]+(i-1)*cskipd+1), float(-55.0))
-        r.add(int(parent[0]+(i-1)*cskipd+1),int(parent[0]), float(-55.0)) 
-for d in range(1,toth-1):
-        cskipd=(1-B*math.pow(B, toth-d-1))/(1-B)
-	cskipdn=(1-B*math.pow(B, toth-d-2))/(1-B)
-	if (d!=toth-1):
-		for i in range(1,int(math.pow(B,d))):
-                        parent.append(d+1)
-			temp=parent[d-1]+(i-1)*cskipd+1
-			for j in range(1,B):
-                        	r.add(int(temp), int(temp+(j-1)*cskipdn+1), float(-55.0))
-                        	r.add(int(temp+(j-1)*cskipdn+1),int(temp), float(-55.0))
-	else:
-                for i in range(1,math.pow(B,d)):
-                        temp=parent[d-1]+(i-1)*cskipd+1
-			for j in range(1,B):
-                       		r.add(int(temp),int(temp+j), float(-55.0))
-                        	r.add(int(temp+j),int(temp), float(-55.0))
+f = open("topo.txt", "r")
+
+lines = f.readlines()
+for line in lines:
+  s = line.split()
+  if (len(s) > 0):
+    print " ", s[0], " ", s[1], " ", s[2];
+    r.add(int(s[0]), int(s[1]), float(s[2]))
                        
 
 totduration=10000
