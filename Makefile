fda413ecccf94c68bb6f61382bc7968452ccb47b
Aqu212 <Aqu212@gmail.com>
2010-11-20 22:17:41 +0000

    modified test.py for the new node id assignment scheme

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 COMPONENT=OMACAppC
-CFLAGS += -DLOW_POWER_LISTENING
-
+#CFLAGS += -DLOW_POWER_LISTENING
+CFLAGS+=-DLPL_ENABLE
 BUILD_EXTRA_DEPS = InitMsg.py
-
+CFLAGS +=-DCC1K_DEF_PRESET=1
 InitMsg.py: InitMessage.h
 	mig python -target=$(PLATFORM) $(CFLAGS) -python-classname=InitMsg InitMessage.h init_msg -o $@
 
--- test.py
+++ test.py
@@ -7,50 +7,12 @@ import math
 #calculate preamble length
 T=300.0
 B=3.0
-toth=9
+toth=4
+NNODES=int(((B**toth)-1)/(B-1))
 
 Perc=0.2
 
-I_listen=3.613636364
-I_rx=15.0
-I_tx=20.0
-#I_tx5=8+14
-#I_tx0=*+17.4
-I_sleep=0.03
-ton=0.0022
-preamblelist=[]
 
-Lpak=36.0
-trxb=416.0*10.0**(-6)
-ttxb=trxb
-tdata=1.1
-dparent=1.0
-h=toth-1
-temp1=(B**h-1)/(B-1)
-temp2=B**h-1
-Numerator=(I_rx-I_sleep)*ton*(temp1+Perc*temp2)
-temp3=(ton-Lpak*trxb)*(temp1+Perc*temp2)
-temp4=ton*(1-dparent)/dparent+Lpak*ttxb
-temp5=temp1*temp4
-Denominator=(I_listen-I_sleep)*(T+temp3-temp5-tdata)
-d=math.sqrt(Numerator/Denominator)
-toff=ton/d-ton
-preamblelist.append(toff)
-dparent=d
-for h in range (toth-2,1,-1):
-	temp1=(B**h-1)/(B-1)
-	temp2=B**h-1+(B**(h+1)-1)/(B-1)
-	Numerator=(I_rx-I_sleep)*ton*(temp1+Perc*temp2)
-	temp3=(ton-Lpak*trxb)*(temp1+Perc*temp2)
-	temp4=ton*(1-dparent)/dparent+Lpak*ttxb
-	temp5=temp1*temp4
-	Denominator=(I_listen-I_sleep)*(T+temp3-temp5-tdata)
-	d=math.sqrt(Numerator/Denominator)
-	toff=ton/d-ton
-	preamblelist.append(toff)
-	dparent=d
-preamblelist.append("always off")
-print str(preamblelist)	
 
 #begin configuration
 channelList=["omacapp","Boot", "TossimPacketModelC"]#, "AM","TossimPacketModelC", "Queue", "Tossim"]
@@ -66,16 +28,25 @@ def initNodes():
   global channelList
   for i in range(len(channelList)):
     t.addChannel(channelList[i], sys.stdout)
+#  for i in range(NNODES):
+#    nodelist[i]=t.getNode(i+1)
+#    nodelist[i].bootAtTime(2*t.ticksPerSecond()+(90*(i+1) + 2 << i))
+  i=0
+  for h in range(toth):
+    for j in range(int(B**h)):
+      nodelist[i]=t.getNode(h*64+j+1)
+      i=i+1
+   
   for i in range(NNODES):
-    nodelist[i]=t.getNode(i+1)
     nodelist[i].bootAtTime(2*t.ticksPerSecond()+(90*(i+1) + 2 << i))
 #    v=nodelist[i].getVariable("OMACC.a")
 #    v.setData(100)
 #    print v.getData()
 
+
 initNodes()
 
-f = open("topo.txt", "r")
+f = open("newtopo.txt", "r")
 lines = f.readlines()
 for line in lines:
   s = line.split()
@@ -89,34 +60,45 @@ for line in lines:
   str1 = line.strip()
   if (str1 != ""):
     val = int(str1)
-    for i in range(1, NNODES+1):
-      t.getNode(i).addNoiseTraceReading(val)
+    #for i in range(1, NNODES+1):
+      #t.getNode(i).addNoiseTraceReading(val)
+    for h in range(toth):
+      for j in range(int(B**h)):
+        t.getNode(h*64+j+1).addNoiseTraceReading(val)
+        
 
-for i in range(1, NNODES+1):
-  print "Creating noise model for ",i;
-  t.getNode(i).createNoiseModel()
+for h in range(toth):
+  for j in range(int(B**h)):
+    print "Creating noise model for ",h*64+j+1
+    t.getNode(h*64+j+1).createNoiseModel()
+
+#for i in range(1, NNODES+1):
+#  print "Creating noise model for ",i;
+#  t.getNode(i).createNoiseModel()
 
 totduration=55555
 
-ret=t.runNextEvent()
-msg=InitMsg()
-flagB=0;
-j=0
-for i in range(1, NNODES+1):
-  msg.set_b(int(B))
-  msg.set_h(toth-j)
-  pkt = t.newPacket();
-  pkt.setData(msg.data)
-  pkt.setType(msg.get_amType())
-  pkt.setDestination(i)
-  print "Delivering " + str(msg) + " to 0 at " + str(t.time());
-  pkt.deliver(i, t.time())
-  flagB=flagB+1
-  if (flagB=B**j):
-    j=j+1
-    flagB=0
+#ret=t.runNextEvent()
+#msg=InitMsg()
+#flagB=0;
+#j=0
+#for i in range(1, NNODES+1):
+#  msg.set_b(int(B))
+#  msg.set_h(toth-j)
+#  pkt = t.newPacket();
+#  pkt.setData(msg.data)
+#  pkt.setType(msg.get_amType())
+#  pkt.setDestination(i)
+#  print "Delivering " + str(msg) + " to 0 at " + str(t.time());
+#  pkt.deliver(i, t.time())
+#  flagB=flagB+1
+#  if (flagB=B**j):
+#    j=j+1
+#    flagB=0
 
-totduration=totduration-1
+
+
+#totduration=totduration-1
 
 while(totduration>0):
   ret=t.runNextEvent()
