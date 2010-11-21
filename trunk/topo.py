35f9eb82a1ebee2e713cb6396be16b170ec872e3
nikhilr1984@gmail.com <nikhilr1984@gmail.com@9b876943-6dad-42c9-a370-2adcb6fe3f21>
2010-11-21 17:00:18 +0000

    generic topo for any B.

--- trunk/topo.py
+++ trunk/topo.py
@@ -3,16 +3,16 @@ B = 3
 n = 1
 gain = -55
 
-#print "h: " + str(0) + "\n"
-#print " node id= " + str(n) + "\n"
-# 
-#for i in range(1,toth):
-#  print "h: " + str(i) + "\n"
-#  t = 0
-#  for j in range(B * n):
-#    print "j: " + str(j) + " node id= " + str(i * 64 + j + 1) + "\n"
-#    t = t + 1
-#  n = t
+print "h: " + str(0) + "\n"
+print " node id= " + str(n) + "\n"
+ 
+for i in range(1,toth):
+  print "h: " + str(i) + "\n"
+  t = 0
+  for j in range(B * n):
+    print "j: " + str(j) + " node id= " + str(i * 64 + j + 1) + "\n"
+    t = t + 1
+  n = t
 
 ftopo = open("newtopo.txt", "w")
 n = 1
@@ -23,9 +23,10 @@ for i in range(1,toth):
     ftopo.write(str((i-1) * 64 + int(j/B) + 1) + " " + str(i * 64 + j + 1) + " " + str(gain) + "\n")
     ftopo.write(str(i * 64 + j + 1) + " " + str((i-1) * 64 + int(j/B) + 1) + " " + str(gain) + "\n")
     # links between siblings
-    if((j % B) > ((j-1) % B)):
-      ftopo.write(str(i * 64 + j) + " " + str(i * 64 + j + 1) + " " + str(gain) + "\n")
-      ftopo.write(str(i * 64 + j + 1) + " " + str(i * 64 + j) + " " + str(gain) + "\n")
+    for k in range(1,B):
+      if(((j+k) > j) and (((j+k) % B) > (j % B))):
+        ftopo.write(str(i * 64 + j+1) + " " + str(i * 64 + j + k + 1) + " " + str(gain) + "\n")
+        ftopo.write(str(i * 64 + j + k + 1) + " " + str(i * 64 + j+1) + " " + str(gain) + "\n")
     t = t + 1
   n = t
 
