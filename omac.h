f5aba555a88ef15a0dfaf179a8bf29db22c659ef
Aqu212 <Aqu212@gmail.com>
2010-12-07 05:04:35 +0000

    hwparser

--- omac.h
+++ omac.h
@@ -4,8 +4,8 @@
 #define MAX_H 4
 #define MAX_NODE_ID 21
 const int16_t HOP[MAX_NODE_ID] = {-1, -1, 1, 3, -1, 2, 1, 3, 3, 3, 2, 3, 2, 2, 3, 0, 3, -1, -1, -1, 3}; 
-//const uint16_t SLEEPTIME[MAX_H] ={0, 100, 100, 65535};
+const uint16_t SLEEPTIME[MAX_H] ={0, 41, 54, 65535};
 
-const uint16_t SLEEPTIME[MAX_H] ={0, 100, 100, 100};
+//const uint16_t SLEEPTIME[MAX_H] ={0, 100, 100, 100};
 
 #endif
