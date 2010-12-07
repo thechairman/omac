7eeb3aaa3e3a45a267906aea4b859a82fadb6475
Aqu212@gmail.com <Aqu212@gmail.com@9b876943-6dad-42c9-a370-2adcb6fe3f21>
2010-12-07 06:07:32 +0000

    add log

--- trunk/omac.h
+++ trunk/omac.h
@@ -4,7 +4,7 @@
 #define MAX_H 4
 #define MAX_NODE_ID 21
 const int16_t HOP[MAX_NODE_ID] = {-1, -1, 1, 3, -1, 2, 1, 3, 3, 3, 2, 3, 2, 2, 3, 0, 3, -1, -1, -1, 3}; 
-const uint16_t SLEEPTIME[MAX_H] ={0, 41, 54, 65535};
+const uint16_t SLEEPTIME[MAX_H] ={0, 11, 15, 65535};
 
 //const uint16_t SLEEPTIME[MAX_H] ={0, 100, 100, 100};
 
